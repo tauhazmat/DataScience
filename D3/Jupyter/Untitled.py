923fbd341afefd7504a1d965d7080a130490f916
Tauha Azmat <160022078+tauhazmat@users.noreply.github.com>
2026-05-29 00:15:17 +0500

    Jupyter & Plot types till scatter plots covered
    Covered Jupyter Notebook (Anaconda) and bar-graph plots covered till scatterplots

---
+++ D3/Jupyter/Untitled.py
@@ -0,0 +1,152 @@
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "id": "f44513fb-e94a-43ec-9f4c-9a46058c231b",
+   "metadata": {},
+   "source": [
+    "# Python Course\n",
+    "## How to use Jupyter notebook\n"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "c25d05f9-f752-4486-a967-dd60ef62fde6",
+   "metadata": {},
+   "source": [
+    "## 1 **First Program**"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 16,
+   "id": "20455d64-2aa2-48a7-a9a0-9bba4a66629e",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "First python program\n",
+      "I love Data Science!\n",
+      "67\n"
+     ]
+    }
+   ],
+   "source": [
+    "print(\"First python program\")\n",
+    "print(\"I love Data Science!\")\n",
+    "print(66+1)\n"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "47653605-c921-4178-93fb-db00f6878ab1",
+   "metadata": {},
+   "source": [
+    "## 2 **Operators**"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 17,
+   "id": "fa7ab46a-65a7-4c9d-9dac-1b85886dfcba",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "3\n",
+      "2\n",
+      "3.0\n",
+      "6\n",
+      "1\n",
+      "3\n",
+      "9\n"
+     ]
+    }
+   ],
+   "source": [
+    "# operators.py\n",
+    "\n",
+    "\n",
+    "print(2 + 1) #add \n",
+    "print(3 - 1) #subtract\n",
+    "print(6 / 2) #float (result of division comes with point)\n",
+    "print(2 * 3) #multiply  \n",
+    "print(13 % 2) #remainder\n",
+    "\n",
+    "print(6 // 2) #divide (result does not come with a point \".\")\n",
+    "print(3**2) #3 power 2\n",
+    "\n",
+    "#Sequence in which Pyhton runs a code 👇🏻\n",
+    "#PEMDAS:\n",
+    "# Parenthesis Exponents Multiply Divide Addition Substraction\n",
+    "# left to right sequence for M D & A S\n"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "d6708f7d-60e9-493b-8d94-6c8947fa810b",
+   "metadata": {},
+   "source": [
+    "## 3 **Strings**"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 18,
+   "id": "0be7a38e-e1c3-41d1-8bf0-f20791950929",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Test for single quotes\n",
+      "Test for double quotes\n",
+      "Test for tripple quotes\n",
+      "what's up?\n"
+     ]
+    }
+   ],
+   "source": [
+    "# print(\"Hello World\")\n",
+    "# print(\"Hello I am Tauha Azmat\")\n",
+    "\n",
+    "print('Test for single quotes')\n",
+    "print(\"Test for double quotes\")\n",
+    "print('''Test for tripple quotes''')\n",
+    "# ''\n",
+    "# \"\"\n",
+    "# ''''''\n",
+    "\n",
+    "print(\"what's up?\")\n",
+    "\n",
+    "# Space counts and shows as it is in a string"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python [conda env:base] *",
+   "language": "python",
+   "name": "conda-base-py"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.12.7"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
