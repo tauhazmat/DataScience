07825faedf3b47561df9197cfe6fcb53cd4615e2
Tauha Azmat <160022078+tauhazmat@users.noreply.github.com>
2026-06-04 13:30:40 +0500

    Implement basic barplot using Titanic dataset
    Added a simple barplot example using the Titanic dataset.

--- D4/main.py
+++ D4/main.py
@@ -1,1 +1,13 @@
 print("This Directory contains Data Visualization Files")
+
+
+
+# How to make a simple barplot (practice)
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+titaic_dataset = sns.load_dataset("titanic")
+titanic
+
+plt.barplot(x= "sex", y="fare", data=titanic)
+# plt.show()  commented out plt.show so preview is not shown
