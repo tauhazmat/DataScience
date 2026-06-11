1702c105f9dbdef7dc346157c768756b5055d488
Tauha Azmat <160022078+tauhazmat@users.noreply.github.com>
2026-06-11 19:36:26 +0500

    Fix barplot display and add Titanic dataset
    Added Titanic dataset visualization with barplot.

--- D4/practice.py
+++ D4/practice.py
@@ -7,4 +7,23 @@ flower_data = sbn.load_dataset("iris")
 flower_data
 
 plt.barplot(x = "petal_length", y = "petal_width", data = flower_data)
-plt.show
+plt.show()
+
+
+
+
+
+
+
+
+
+
+# Titanic dataset
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+titaic_dataset = sns.load_dataset("titanic")
+titanic
+
+plt.barplot(x= "sex", y="fare", data=titanic)
+plt.show() # Preview shown
